7dba04b95f1e565c692b869b2a6d0dca487eb5cd
Jejcofa <bogdankuskov@gmail.com>
2024-12-17 21:18:38 +0200

    Added HW4.1

---
+++ HW4.1/Program.cs
@@ -0,0 +1,22 @@
+﻿string choice, day;
+Console.WriteLine("Введите номер дня недели:");
+choice = Console.ReadLine();
+
+if (choice == "1")
+    day = "Понедельник";
+else if (choice == "2")
+    day = "Вторник";
+else if (choice == "3")
+    day = "Среда";
+else if (choice == "4")
+    day = "Четверг";
+else if (choice == "5")
+    day = "Пятница";
+else if (choice == "6")
+    day = "Суббота";
+else if (choice == "7")
+    day = "Воскресенье";
+else
+    day = "Неверное число! (1-7)";
+
+Console.WriteLine($"Ваш день: {day}");
