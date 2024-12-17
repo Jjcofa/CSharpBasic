5eb1d3dae7efbd04b06b7c541bff7b61fcfe012d
Jejcofa <bogdankuskov@gmail.com>
2024-12-17 21:02:49 +0200

    Added HW 4.2
    Calculator

---
+++ HW4.2/Program.cs
@@ -0,0 +1,31 @@
+﻿string oper;
+double result, numOne, numTwo;
+
+Console.WriteLine("Выберите действие(+, -, /, *):");
+oper = Console.ReadLine();
+Console.WriteLine("Введите первое число:");
+numOne = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+numTwo = Convert.ToDouble(Console.ReadLine());
+
+switch (oper)
+{
+	case "+":
+		result = numOne + numTwo;
+		Console.WriteLine($"{result}");
+		break;
+
+    case "-":
+		result = numOne - numTwo;
+        Console.WriteLine($"{result}");
+        break;
+
+    case "*":
+        result = numOne * numTwo;
+        Console.WriteLine($"{result}");
+        break;
+    case "/":
+        result = numOne / numTwo;
+        Console.WriteLine($"{result}");
+        break;
+}
