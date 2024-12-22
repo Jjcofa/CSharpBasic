54aabb62711e77d2b7c324a34c57f9877659e9c9
Jejcofa <bogdankuskov@gmail.com>
2024-12-23 01:09:49 +0200

    Added HW5.1 and HW5.2

---
+++ HW5.1/Program.cs
@@ -0,0 +1,24 @@
+﻿string firstNum, secondNum;
+int dividend, divisor, result;
+
+Console.WriteLine("Введите первое число:");
+firstNum = Console.ReadLine();
+Console.WriteLine("Введите второе число:");
+secondNum = Console.ReadLine();
+
+
+try
+{
+    dividend = int.Parse(firstNum);
+    divisor = int.Parse(secondNum);
+    result = dividend / divisor;
+    Console.WriteLine($"Результат: {result}.");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Деление на ноль невозможно.");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Некорректное число.");
+}
---
+++ HW5.2/Program.cs
@@ -0,0 +1,15 @@
+﻿string input;
+int number;
+
+Console.WriteLine("Введите число:");
+input = Console.ReadLine();
+
+try
+{
+    number = int.Parse(input);
+    Console.WriteLine($"Вы ввели число: {number}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Вы ввели некорректное число.");
+}
