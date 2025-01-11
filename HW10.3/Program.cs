254de4d5790bdc6c01834ca491f40b16c9523dda
Jejcofa <bogdankuskov@gmail.com>
2025-01-11 15:42:44 +0200

    Added HW10.3

---
+++ HW10.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+Console.WriteLine("Введите текст:");
+string input = Console.ReadLine();
+
+StringBuilder sb = new StringBuilder();
+
+foreach (char c in input)
+{
+    if (c != ' ')
+    {
+        sb.Append(c);
+    }
+}
+
+Console.WriteLine("Текст без пробелов:");
+Console.WriteLine(sb.ToString());
