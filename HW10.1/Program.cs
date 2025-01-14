3ef50dd47f6ef5a12a896a0a36b8d239ab8380cb
Jejcofa <bogdankuskov@gmail.com>
2025-01-14 18:30:08 +0200

    Reworked HW10.1

--- HW10.1/Program.cs
+++ HW10.1/Program.cs
@@ -1,28 +1,17 @@
 ﻿using System;
 using System.Text;
+
 Console.OutputEncoding = Encoding.UTF8;
-
-//Console.WriteLine("Введіть прізвище та ім’я:");
-//string[] name = Console.ReadLine().Split(' ');
-
-//if (name[0][0] == name[1][0])
-//{
-//    Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
-//}
-//else
-//{
-//    Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
-//}
-
 
 Console.WriteLine("Введіть прізвище та ім’я:");
 string name = Console.ReadLine();
 
-int spaceIndex = name.IndexOf(' ');
-if (spaceIndex != -1)
+string[] parts = name.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+if (parts.Length == 2)
 {
-    char firstLetterSurname = name[0];
-    char firstLetterName = name[spaceIndex + 1];
+    char firstLetterSurname = parts[0][0]; 
+    char firstLetterName = parts[1][0];
 
     if (firstLetterSurname == firstLetterName)
     {
@@ -33,7 +22,8 @@ if (spaceIndex != -1)
         Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
     }
 }
+
 else
 {
-    Console.WriteLine("Помилка: введіть прізвище та ім’я через пробіл.");
+    Console.WriteLine("Невірний формат");
 }
