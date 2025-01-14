ebd8ae8d92587ead3ea543f1ec9a4bb2b4bb8ed2
Jejcofa <bogdankuskov@gmail.com>
2025-01-14 17:04:50 +0200

    Added HW10.1

---
+++ HW10.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+//Console.WriteLine("Введіть прізвище та ім’я:");
+//string[] name = Console.ReadLine().Split(' ');
+
+//if (name[0][0] == name[1][0])
+//{
+//    Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
+//}
+//else
+//{
+//    Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
+//}
+
+
+Console.WriteLine("Введіть прізвище та ім’я:");
+string name = Console.ReadLine();
+
+int spaceIndex = name.IndexOf(' ');
+if (spaceIndex != -1)
+{
+    char firstLetterSurname = name[0];
+    char firstLetterName = name[spaceIndex + 1];
+
+    if (firstLetterSurname == firstLetterName)
+    {
+        Console.WriteLine("Прізвище починається на ту ж літеру, що і ім’я");
+    }
+    else
+    {
+        Console.WriteLine("Прізвище не починається на ту ж літеру, що і ім’я");
+    }
+}
+else
+{
+    Console.WriteLine("Помилка: введіть прізвище та ім’я через пробіл.");
+}
