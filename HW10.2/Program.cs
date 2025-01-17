76064504087f76bb0a0020648d51221148ba76d6
Jejcofa <bogdankuskov@gmail.com>
2025-01-17 17:10:53 +0200

    Added HW10.2

---
+++ HW10.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+StringBuilder report = new StringBuilder();
+
+Console.WriteLine("Введіть заголовок звіту:");
+string title = Console.ReadLine();
+
+report.AppendLine(title);
+report.AppendLine($"Дата: {DateTime.Now:dd.MM.yyyy}");
+report.AppendLine("Список подій:");
+
+while (true)
+{
+    Console.WriteLine("Введіть подію (або натисніть Enter для завершення):");
+    string eventInput = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(eventInput))
+    {
+        break;
+    }
+
+    report.AppendLine($"* {eventInput}");
+}
+
+Console.WriteLine("\nЗвіт:");
+Console.WriteLine(report.ToString());
